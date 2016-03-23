4fde4a34ef31c0545c39cb6d01c5f7cba318d766
yingQiangShen <1528787321@qq.com>
2016-03-23 10:49:47 +0800

    Initial Commit

---
+++ 工厂CellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  工厂CellDemo
+//
+//  Created by syq on 16/3/23.
+//  Copyright © 2016年 lanou.syq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 工厂CellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  工厂CellDemo
+//
+//  Created by syq on 16/3/23.
+//  Copyright © 2016年 lanou.syq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
